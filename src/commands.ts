89893e74d1d78148831b43d8774c39ae195bc498
PhuongHieuHo <fuhi.dev@gmail.com>
2021-09-16 18:46:00 +0700

    .

--- src/commands.ts
+++ src/commands.ts
@@ -4,4 +4,8 @@ import { CommandType } from './enums/command-type';
 
 export const commandsMap = new Map<CommandType, ICommand>([
   [CommandType.Module, { fileName: 'my-module', resource: ResourceType.Module }],
+  [CommandType.Entity, { fileName: 'my-entity', resource: ResourceType.Entity }],
+  [CommandType.Controller, { fileName: 'my-controller', resource: ResourceType.Controller }],
+  [CommandType.Full, { fileName: 'my-module', resource: ResourceType.Full }],
+  [CommandType.Service, { fileName: 'my-service', resource: ResourceType.Service }],
 ]);
